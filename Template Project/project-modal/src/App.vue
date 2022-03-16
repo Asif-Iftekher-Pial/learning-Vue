3b21b903c53a7a6e5aeae125958bffc05d46ada4
Asif Iftekher Pial <iftekherpial67@gmail.com>
2022-03-16 21:26:36 +0600

    Learned upto Vue CLI Lecture number 5

---
+++ Template Project/project-modal/src/App.vue
@@ -0,0 +1,90 @@
+<template>
+  <h1>{{ title }}</h1>
+  <br />
+  <input type="text" class="active" ref="boo" />
+  <br />
+  <button @click="handleClick">Highlight TextBox</button>
+  <!-- calling the customModal in html template -->
+  <!-- : colon is for multiple props binding -->
+  <div v-if="showModal">
+    <Modal theme="sale" @close="toggleModal">
+      <!-- slots is used to pass more element to customModal -->
+      <!-- default slot -->
+      <h3>this is slots info</h3>
+      <p>Lorem, ipsum.</p>
+      <!-- specific slot contain v-slot and the name ->links  -->
+      <template v-slot:links>
+        <a href="#">Sign up Now</a>
+
+        <a href="#">More info</a>
+      </template>
+    </Modal>
+    <!-- @close is the custome click event created in customModal  which is calling toggleModal function -->
+  </div>
+
+   <div v-if="showModalTwo">
+    <Modal  @close="toggleModaltwo">
+      <!-- slots is used to pass more element to customModal -->
+      <!-- default slot -->
+      <h3>this is Modal Two</h3>
+      <p>Lorem, ipsum dolor.</p>
+      
+    </Modal>
+    <!-- @close is the custome click event created in customModal  which is calling toggleModal function -->
+  </div>
+
+  
+  <button @click="toggleModal">show Modal</button>
+
+  <button @click="toggleModaltwo">Show Modal Two</button>
+</template>
+
+<script>
+import Modal from "./components/customModal.vue";
+export default {
+  // register the customModal
+  components: { Modal},
+  name: "App",
+  data() {
+    return {
+      title: "My first vue app",
+
+      showModal: false,
+      showModalTwo: false,
+    };
+  },
+  methods: {
+    handleClick() {
+      //  console.log(this.$refs.boo); // $refs is used for selecting any refs in template html file
+      this.$refs.boo.classList.toggle("active"); // will add active class
+      this.$refs.boo.focus();
+    },
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+    toggleModaltwo() {
+      // alert('oke')
+      this.showModalTwo = !this.showModalTwo;
+    },
+  },
+};
+</script>
+
+<style>
+button{
+  margin-right: 5px;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid red;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+</style>
---
+++ Template Project/project-modal/src/components/customModal.vue
@@ -0,0 +1,71 @@
+<template>
+  <!-- self indicates that , if click occurs in backdrop class it will not work-->
+  <div class="backdrop" @click.self="closeModal">
+    <!-- adding class name sale via props -->
+    <div class="modal" :class="{ sale: theme === 'sale' }">
+      <slot></slot>
+
+      <div class="actions">
+        <!-- specific name slot -->
+        <slot name="links"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+//registering or  passing object of props from app.vue ....this section will recieve the props
+export default {
+  props: ["theme"], //  recieving props obj
+  methods: {
+    //closing the modal
+    closeModal() {
+      this.$emit(
+        "close"
+      ); /* $emit is a custom event we are creating with the name 'close'
+            which will recieve by parrent component App.vue */
+    },
+  },
+};
+</script>
+
+<style >
+.modal .actions {
+  margin: 30px 0 10px 0;
+  text-align: center;
+}
+.actions a {
+  color: rgb(5, 5, 5);
+  padding: 8px;
+  border: 1px solid rgb(184, 41, 41);
+  border-radius: 4px;
+  text-decoration: none;
+  margin: 10px;
+}
+.modal {
+  width: 40%;
+  padding: 20px;
+  margin: 100px auto;
+  background: white;
+  border-radius: 10px;
+}
+.backdrop {
+  top: 0;
+  position: fixed;
+  background: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+}
+h1 {
+  color: aqua;
+  border: none;
+  padding: 0;
+}
+.modal.sale {
+  background: crimson;
+  color: white;
+}
+.modal.sale.h1 {
+  color: white;
+}
+</style>
